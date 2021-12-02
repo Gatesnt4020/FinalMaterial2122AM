d47d2e71fd8c46a46c0724fec77e58100cac0cdd
Gatesnt4020 <91961352+Gatesnt4020@users.noreply.github.com>
2021-12-01 21:06:55 -0600

    Update Krabs+.py

--- Krabs+.py
+++ Krabs+.py
@@ -1,146 +1,17 @@
-orderInfo = "\nOrder1:\n"
-total=0
-orderTotal=0
-def menuFries(question,choice,size,price):
-     global orderInfo
-     global orderTotal
-     option = input(question)
-     if option == "y":
-          for i in choice:
-               print(i)
-          option = input("Which choice? ")
-          while option != choice[0] and option != choice[1]:
-               for i in choice:
-                    print(i)
-               option = input("Which choice? ")
-          orderInfo+=(f"\t{option} ")
-          if option == choice[0]:
-               for i in size:
-                    print(i)
-               option = input("Which size? ")
-               while option != [0] and option != [1] and option != [3]:
-                    for i in size:
-                         print(i)
-                    option = input("Which size? ")
-               if option == size[0]:
-                    orderTotal+=price[0]
-               elif option == size[1]:
-                    orderTotal+=price[1]
-               elif option == size[2]:
-                    orderTotal+=price[2]
-               elif option ==size[3]:
-                    orderTotal+=price[3]
-               orderInfo+=(f"{option}\n")
-          elif option ==  choice[1]:
-               option = input("Do you want salty sauce with that? (y/n) ")
-               while option != "y" and option != "n":
-                    option = input("Do you want salty sauce with that? (y/n) ")
-               if option == "y":
-                    orderTotal+=price+.5
-                    orderInfo+=(f"w/ salty sauce\n")
-               elif option == "n":
-                    orderTotal+=price
-                    orderInfo+=(f"w/o salty sauce\n")
-                    
-def menuDrink(question,choice,size,price):
-     global orderInfo
-     global orderTotal
-     option = input(question)
-     if option == "y":
-          for i in choice:
-               print(i)
-          option = input(f"Which choice? {choice} ")
-          while option != choice[0] and option != choice[1]:
-               option = input(f"Which choice? {choice} ")
-          orderInfo+=(f"\t{option} ")
-          if option == choice[0]:
-               for i in size:
-                    print(i)
-               option = input("Which size? ")
-               while option != size[0] and option != size[1] and option != size[2]:
-                    option = input("Which size? ")
-                    for i in size:
-                         print(i)
-               if option == size[0]:
-                    orderTotal+=price[0]
-               elif option == size[1]:
-                    orderTotal+=price[1]
-               elif option == size[2]:
-                    orderTotal+=price[2]
-               elif option ==size[3]:
-                    orderTotal+=price[3]
-               orderInfo+=(f"{option}\n")
-          elif option ==  choice[1]:
-               orderTotal+=price[4]
-               
-def menuBurger(question,choice,price):
-     global orderTotal
-     global orderInfo
-     option = input(question)
-     if option == "y":
-          for i in choice:
-               print(i)
-          option = input("Which choice? ")
-          while option != choice[0] and option != choice[1] and option != choice[2]:
-               for i in choice:
-                    print(i)
-               option = input(f"Which choice? ")
-          orderInfo+=(f"\t{option} ")
-          if option == choice[0]:
-               orderTotal+=price[0]
-               option = input("Would you like sea cheese with that? (y/n) ")
-               while option != "y" and option != "n":
-                    option = input("Would you like sea cheese with that? (y/n) ")
-               if option =="y":
-                    orderTotal+=price[3]
-                    orderInfo+=(f"w/ sea cheese\n")
-               else:
-                    orderInfo+=(f"w/o sea cheese\n")
-          elif option == choice[1]:
-               orderTotal+=price[1]
-               option = input("Would you like sea cheese with that? (y/n) ")
-               while option != "y" and option != "n":
-                    option = input("Would you like sea cheese with that? (y/n) ")
-               if option =="y":
-                    orderTotal+=price[3]
-                    orderInfo+=(f"w/ sea cheese\n")
-               else:
-                    orderInfo+=(f"w/o sea cheese\n")
-          elif option == choice[2]:
-               orderTotal+=price[2]
-               option = input("Would you like sea cheese with that? (y/n) ")
-               while option != "y" and option != "n":
-                    option = input("Would you like sea cheese with that? (y/n) ")
-               if option =="y":
-                    orderTotal+=price[3]
-                    orderInfo+=(f"w/ sea cheese\n")
-               else:
-                    orderInfo+=(f"w/o sea cheese\n")
-               
-def menuFoods(question,choice,price):
-     global orderInfo
-     global orderTotal
-     option = input(question)
-     if option == "y":
-          for i in choice:
-               print(i)
-          option = input("Which choice? ")
-          while option != choice[0] and option != choice[1] and option != choice[3] and option != choice[4] and option != choice[5] and option != choice[6]:
-               option = input(f"Which choice? {choice} ")
-          orderInfo+=(f"\t{option}")
-          if option == choice[0]:
-               orderTotal+=price[0]
-          elif option == choice[1]:
-               orderTotal+=price[1]
-          elif option == choice[2]:
-               total+=price
-     
+from Order import Order
+menu=["Krabby Patty","Double Krabby Patty","Triple Krabby Patty","Seafoam Soda","Kelp Shake","Coral Bits","Kelp Rings","Krabby Meal", "Double Krabby Meal","Triple Krabby Meal","Salty Sea Dog","Footlong","Sailors Surprise","Golden Loaf"]
+masterOrder=[]
+
+def showMenu():
+     for i in menu:
+          print(i)
+
 greeting = (f"""What would you like to do?
 "Menu" - See what the menu is
 "Order" - Order something from the menu
-"New Order" - Make a new order
-"Edit Order" - Edit an order that has already been made
-"Remove Order" - Remove an order that has already been made
+"New order" - Make a new order
+"Edit order" - Edit an order that has already been made
+"Remove order" - Remove an order that has already been made
 "Quit" - Finalize your order(s) and leave
 """)
 
@@ -148,6 +19,48 @@ ui = input("Welcome to the Krusty Krab would you like something today?(y/n) ")
 while ui != "y" and ui != "n":
      ui = input("Welcome to the Krusty Krab would you like something today?(y/n) ")
 if ui == "y":
+     while ui !="Quit":
           ui = input(greeting)
+          if ui == "Menu":
+               showMenu()
+          elif ui == "Order":#use the menuChecker on this 
+               addFood = input("What do you want to add")
+               #look at static method to d othis
+               food = Order(addFood)
+               masterOrder.append(food)
+          elif ui == "New order":
+               if masterOrder==[]:
+                    print("Please order something before adding another order")
+               else:
+                    masterOrder.append([])
+                    print("Add to your next order")
+          elif ui == "Edit order":#look order comment
+               if masterOrder==[]:
+                    print("Please order something before editing an order")
+               else:
+                     ui = input("Which order would you like to edit")
+                     while(not ui.isdigit()):
+                         ("Which order would you like to edit")
+                     for i in range(len(masterOrder)):
+                         if masterOrder[i]==ui:
+                              print(masterOrder[ui])
+                              #do the rest of editing prb like adding
+          elif ui == "Remove order":
+               if masterOrder==[]:
+                    print("Please order something before removing an order")
+               else:
+                     ui = input("Which order would you like to remove")
+                     while(not ui.isdigit()):
+                         ui = input("Which order would you like to remove")
+                     ui = int(ui)
+                     for i in range(len(masterOrder)):
+                         if masterOrder[i]==ui:
+                              del masterOrder[i]
+                              break
+          ui = input(greeting)
+     #print receipt  here
 elif ui == "n":
-     print("Take you for coming have a nice day")
+     print("Thank you for coming have a nice day")
+
+#use id()
+#  for encryption or something
