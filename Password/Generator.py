c65892cbf50224513479edb2344f5b3e49971de0
Gatesnt4020 <christian.gates@evsck12.com>
2021-12-05 19:45:11 -0600

    here figure the rest out

--- Password/Generator.py
+++ Password/Generator.py
@@ -2,23 +2,7 @@ import random,string
 upper = string.ascii_uppercase
 lower = string.ascii_lowercase
 numbers = string.digits
-symbols = string.punctuation
-
-inProgram=True
-print("Welcome to the program to help remember all of the passwords in the world")
-ui = input("Are you a new user (y/n) ")
-while ui != "y" and "n" : ui = input("Are you a new user (y/n) ")
-while inProgram == True:
-    if ui == "y":
-        First=input("Enter your first name. ").upper()
-        while (not First.isalpha()) : First=input("Enter your first name ").upper()
-        Last=input("Enter your last name ").upper
-        while (not Last.isalpha()) : Last=input("Enter your last name ").upper
-        User=input("Enter your username ").upper
-        while (User != chr(33,126)) : User=input("Enter your username without spaces ").upper
-        '''while (not User.isalpha() or not User.isdigit()) : User=input("Enter your username ").upper'''
-        password=input("Enter a password requirements: 8 characters, 1 upper, 1 lower, and includes a special character")
-        while (8<=len(password)) : print("")
+symbols = "!@#$%^&*()"
 
 def generate(cap, low, num, sym):
     password = ""
---
+++ Password/PasswordManager.py
@@ -0,0 +1,69 @@
+from NewUser import NewUser
+import io
+ui = ""
+
+def makeChecker(filename):
+    for i in usered:
+        listOfList.append([])
+    with open(filename,"r") as file:
+        #readlines returns a list
+        listOflines= file.readlines()
+    for row in listOflines:
+        for i in range(len(usered)):
+            if usered[i] in row:
+                listOfList[i].append(row)  
+
+print("Need help remembering all of the passwords in the world? This program will save all of the passwords you need for your computer.")
+while ui != "Quit":
+    ui = input(f'''are you a (new) user or are you (login) in: ''').lower()
+    if ui == "new":
+        NewUser.makeUser()
+    elif ui == "login":
+        trys = 3
+        while trys != 0:
+            usered = []
+            listOfList = []
+            username = input("Enter a username: ")
+            usered.append(username)
+            password = input("Enter a password: ")
+            usered.append(password)
+            filename = "users.txt"
+            makeChecker(filename)
+            filename = "passes.txt"
+            makeChecker(filename)
+            listOfList = str(listOfList)
+            print(listOfList)
+            usered = str(usered)
+            listOfList = listOfList.replace("[","")
+            listOfList = listOfList.replace("]","")
+            listOfList = listOfList.replace("\\n","")
+            listOfList = listOfList.pop()
+            print(listOfList)
+            with io.StringIO(listOfList) as uname:
+                num = 0
+                wordIndex={}
+                for row in uname:
+                    word = row.strip()
+                    if word not in wordIndex:
+                        wordIndex[word]
+                    num+=1
+            with io.StringIO(usered) as pasd:
+                for row in pasd:
+                    word = row.strip()
+                    if word not in wordIndex:
+                        print("This is the wronge username or password")
+                        trys-=1
+                    else:
+                        print("You have succesfully login in :)")
+        if trys == 0:
+            exit()
+file = open(f"{username}.txt","r+")
+file.truncate(0)
+file.close()
+#make an account.txt to store the users names then use a password.txt and for checking look to see 
+#if (with a for loop) the username and password match
+#check and see if the username and password is there if not automatically subtract 1
+#maybe check and see first if their there and if they are then running the for loop
+'''Sources:
+    https://stackoverflow.com/questions/43147534/iterate-over-file-and-get-word-index-from-other-file
+    '''
